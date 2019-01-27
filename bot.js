730ae536808c8fbc69be5479bd21048f1df280a6
harony1122 <45592826+harony1122@users.noreply.github.com>
2019-01-27 08:30:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -413,7 +413,7 @@ coll.first().delete()
 client.on('ready', function(){
   client.user.setStatus("dnd");
     var ms = 40000 ;	
-    var setGame = ['!inv','!help'];	
+    var setGame = ['Pixel THE BEST','by Haron'];	
     var i = -1;	
     var j = 0;	
     setInterval(function (){	
