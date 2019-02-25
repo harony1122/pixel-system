5120ce3c39df0d75972c0b6e1ddee4213dceddc2
harony1122 <45592826+harony1122@users.noreply.github.com>
2019-02-26 02:24:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3108,41 +3108,6 @@ client.on("guildMemberAdd", member => { member.createDM().then(function (channel
 
 
 
-client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
- 
-  message.channel.createInvite({
-        thing: true,
-        maxUses: 5,
-        maxAge: 86400
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-                .setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(`
-**
----------------------
--[${message.guild.name}]  هذا هو رابط سيرفر
----------------------
--هذا الرابط صالح للأستخدام 5 مرات 
----------------------
--هذا الرابط صالح لمده 24 ساعه 
----------------------
-**`)
-      message.author.sendEmbed(Embed11)
-    }
-});
-
-
-
-
-
 
 
 
